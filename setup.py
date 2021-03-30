317937e7a6193607407946dd834af64bd2ec9f7b
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-03-30 18:51:50 +0100

    Add pytest

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='code-for-action-test',
     version='1.0.0',
+    install_requires=[
+        'pytest'
+    ],
     packages=find_packages()
 
 )
